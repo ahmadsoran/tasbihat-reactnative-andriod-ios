50d149a4a37e413f046d2e9636d4a3e2803f6f4f
ahmadsoran <ahmadsorann@GMAIL.COM>
2022-05-17 05:11:33 +0300

    android crash fix

--- App.js
+++ App.js
@@ -4,9 +4,11 @@ import { store } from "./app/store";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ThemeProvider } from "@rneui/themed";
 import { setStatusBarStyle } from 'expo-status-bar'
-import { useColorScheme } from "react-native";
+import { PermissionsAndroid, useColorScheme } from "react-native";
 import { useEffect } from "react";
 import { loadAsync } from 'expo-font'
+import * as Updates from 'expo-updates';
+
 import './lang/i18n';
 
 
@@ -24,7 +26,31 @@ export default function App() {
     loadAsync({
       'DigitalFont': require('./assets/fonts/digital-7.ttf'),
     })
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.VIBRATE).then(granted => {
+      if (granted) {
+        console.log('Vibrate permission granted')
+      } else {
+        console.log('Vibrate permission denied')
+      }
+
+    }
+    )
+
   }, [])
+  async () => {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        console.log('update availabile')
+        await Updates.fetchUpdateAsync();
+        // ... notify user of update ...
+        await Updates.reloadAsync();
+
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
   return (
     <Provider store={store}>
       <ThemeProvider>
